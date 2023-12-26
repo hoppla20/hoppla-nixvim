083224ddce186e895a14437cfad910bb82172ddc
vincent.cui <privat@vincentcui.de>
2023-12-26 21:31:28 +0100

    feat: add lspsaga

--- nix/hoppla/nixvimModules/plugins/lsp.nix
+++ nix/hoppla/nixvimModules/plugins/lsp.nix
@@ -23,26 +23,86 @@ in {
         enable = true;
 
         keymaps = {
-          diagnostic = {
-            "<leader>j" = "goto_next";
-            "<leader>k" = "goto_prev";
-          };
+          # disable default lsp diagnostic keymaps
+          # use lspsaga
+          diagnostic = {};
           lspBuf = {
-            "K" = "hover";
             "gD" = "references";
             "gd" = "definition";
             "gi" = "implementation";
             "gt" = "type_definition";
             "<leader>lf" = "format";
-            "<leader>la" = "code_action";
           };
         };
       };
+
+      # https://nvimdev.github.io/lspsaga/
+      lspsaga = {
+        enable = true;
+        definition = {
+          width = 0.618;
+          height = 0.618;
+        };
+        lightbulb = {
+          sign = false;
+        };
+      };
+
+      lsp-lines.enable = true;
 
       lsp-format = {
         enable = true;
         lspServersToEnable = "all";
       };
     };
+
+    keymaps = let
+      prefix = "<leader>l";
+    in [
+      {
+        key = "K";
+        action = ":Lspsaga hover_doc<cr>";
+      }
+      {
+        key = "<leader>j";
+        action = ":Lspsaga diagnostic_jump_next<cr>";
+      }
+      {
+        key = "<leader>k";
+        action = ":Lspsaga diagnostic_jump_prev<cr>";
+      }
+      {
+        key = "${prefix}a";
+        action = ":Lspsaga code_action<cr>";
+      }
+      {
+        key = "${prefix}f";
+        action = ":Lspsaga finder<cr>";
+      }
+      {
+        key = "${prefix}d";
+        action = ":Lspsaga peek_definition<cr>";
+      }
+      {
+        key = "${prefix}t";
+        action = ":Lspsaga peek_type_definition<cr>";
+      }
+      {
+        key = "${prefix}r";
+        action = ":Lspsaga rename<cr>";
+      }
+      {
+        key = "${prefix}o";
+        action = ":Lspsaga outline<cr>";
+      }
+      {
+        key = "${prefix}h";
+        action = ":Lspsaga incoming_calls<cr>";
+      }
+      {
+        key = "${prefix}H";
+        action = ":Lspsaga outgoing_calls<cr>";
+      }
+    ];
   };
 }
