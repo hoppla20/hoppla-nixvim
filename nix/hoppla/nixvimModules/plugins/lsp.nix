39265ab05508ba3b9cd32ea33c6294a03b5ebd7f
vincent.cui <privat@vincentcui.de>
2023-12-26 20:44:08 +0100

    feat: enable lsp format on save and add lsp format mappings

--- nix/hoppla/nixvimModules/plugins/lsp.nix
+++ nix/hoppla/nixvimModules/plugins/lsp.nix
@@ -18,21 +18,30 @@ in {
   };
 
   config = mkIf cfg.self.enable {
-    plugins.lsp = {
-      enable = true;
+    plugins = {
+      lsp = {
+        enable = true;
 
-      keymaps = {
-        diagnostic = {
-          "<leader>j" = "goto_next";
-          "<leader>k" = "goto_prev";
+        keymaps = {
+          diagnostic = {
+            "<leader>j" = "goto_next";
+            "<leader>k" = "goto_prev";
+          };
+          lspBuf = {
+            "K" = "hover";
+            "gD" = "references";
+            "gd" = "definition";
+            "gi" = "implementation";
+            "gt" = "type_definition";
+            "<leader>lf" = "format";
+            "<leader>la" = "code_action";
+          };
         };
-        lspBuf = {
-          "K" = "hover";
-          "gD" = "references";
-          "gd" = "definition";
-          "gi" = "implementation";
-          "gt" = "type_definition";
-        };
+      };
+
+      lsp-format = {
+        enable = true;
+        lspServersToEnable = "all";
       };
     };
   };
