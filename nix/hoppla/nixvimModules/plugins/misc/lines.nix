d6eb7abaf478bb5a03c8821b49fc1319dc64b8c1
vincent.cui <privat@vincentcui.de>
2023-12-28 16:57:36 +0100

    refactor: move buffer keymaps to hoppla.plugins.misc.lines module

--- nix/hoppla/nixvimModules/plugins/misc/lines.nix
+++ nix/hoppla/nixvimModules/plugins/misc/lines.nix
@@ -28,5 +28,56 @@ in {
         enable = true;
       };
     };
+
+    keymaps = [
+      {
+        key = "<M-,>";
+        action = ":BufferLineCyclePrev<cr>";
+        options = {
+          silent = true;
+          desc = "Switch to next buffer";
+        };
+      }
+      {
+        key = "<M-.>";
+        action = ":BufferLineCycleNext<cr>";
+        options = {
+          silent = true;
+          desc = "Switch to previous buffer";
+        };
+      }
+      {
+        key = "<M-S-,>";
+        action = ":BufferLineMovePrev<cr>";
+        options = {
+          silent = true;
+          desc = "Move current buffer to left";
+        };
+      }
+      {
+        key = "<M-S-.>";
+        action = ":BufferLineMoveNext<cr>";
+        options = {
+          silent = true;
+          desc = "Move current buffer to right";
+        };
+      }
+      {
+        key = "${cfg.root.leaderPrefixes.buffers}p";
+        action = ":BufferLinePick<cr>";
+        options = {
+          silent = true;
+          desc = "Pick buffer";
+        };
+      }
+      {
+        key = "<M-q>";
+        action = ":bdelete<cr>";
+        options = {
+          silent = true;
+          desc = "Delete current buffer";
+        };
+      }
+    ];
   };
 }
