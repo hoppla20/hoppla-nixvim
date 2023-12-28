1239f786a02fc4a889ea553bb366a4bc225a2f63
vincent.cui <privat@vincentcui.de>
2023-12-28 10:28:55 +0100

    feat: add treesitter-context and treesitter-refactor

--- nix/hoppla/nixvimModules/plugins/languageSupport/treesitter.nix
+++ nix/hoppla/nixvimModules/plugins/languageSupport/treesitter.nix
@@ -16,20 +16,37 @@ in {
   };
 
   config = mkIf cfg.self.enable {
-    plugins.treesitter = {
-      enable = true;
-      indent = true;
-      folding = true;
-      nixGrammars = true;
-      ensureInstalled = "all";
-
-      incrementalSelection = {
+    plugins = {
+      treesitter = {
         enable = true;
-        keymaps = {
-          initSelection = "<CR>";
-          scopeIncremental = "<CR>";
-          nodeIncremental = "<TAB>";
-          nodeDecremental = "<S-TAB>";
+        indent = true;
+        folding = true;
+        nixGrammars = true;
+        ensureInstalled = "all";
+
+        incrementalSelection = {
+          enable = true;
+          keymaps = {
+            initSelection = "<CR>";
+            scopeIncremental = "<CR>";
+            nodeIncremental = "<TAB>";
+            nodeDecremental = "<S-TAB>";
+          };
+        };
+      };
+
+      treesitter-context = {
+        enable = true;
+        maxLines = 5;
+        minWindowHeight = 20;
+      };
+
+      treesitter-refactor = {
+        enable = true;
+        highlightCurrentScope.enable = true;
+        smartRename = {
+          enable = true;
+          keymaps.smartRename = "grr";
         };
       };
     };
