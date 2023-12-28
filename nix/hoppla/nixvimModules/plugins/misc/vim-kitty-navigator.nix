e37f4e3663535c5cb7cf418c07cb84be10b7c8fd
vincent.cui <privat@vincentcui.de>
2023-12-28 17:07:30 +0100

    feat: change vim-kitty-navigator keybindings to alt+<direction>

--- nix/hoppla/nixvimModules/plugins/misc/vim-kitty-navigator.nix
+++ nix/hoppla/nixvimModules/plugins/misc/vim-kitty-navigator.nix
@@ -17,5 +17,29 @@ in {
 
   config = mkIf cfg.self.enable {
     extraPlugins = [nixpkgs.vimPlugins.vim-kitty-navigator];
+
+    globals.kitty_navigator_no_mappings = 1;
+    keymaps = [
+      {
+        key = "<M-j>";
+        action = ":KittyNavigateDown<cr>";
+        options.silent = true;
+      }
+      {
+        key = "<M-k>";
+        action = ":KittyNavigateUp<cr>";
+        options.silent = true;
+      }
+      {
+        key = "<M-h>";
+        action = ":KittyNavigateLeft<cr>";
+        options.silent = true;
+      }
+      {
+        key = "<M-l>";
+        action = ":KittyNavigateRight<cr>";
+        options.silent = true;
+      }
+    ];
   };
 }
