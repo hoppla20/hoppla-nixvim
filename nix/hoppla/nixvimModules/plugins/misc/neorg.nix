29171e9ae2a99cab29eb15be2c4fb78bd6b94ff1
vincent.cui <privat@vincentcui.de>
2023-12-31 14:48:04 +0100

    feat: update neorg config

--- nix/hoppla/nixvimModules/plugins/misc/neorg.nix
+++ nix/hoppla/nixvimModules/plugins/misc/neorg.nix
@@ -1,4 +1,7 @@
-{nixvim}: {
+{
+  nixpkgs,
+  nixvim,
+}: {
   lib,
   cfg,
   ...
@@ -12,29 +15,64 @@
     ;
 
   inherit (nixvim.lib) helpers;
+
+  localLeaderKeymap = desc: key: lib.nameValuePair "<localleader>${key}" desc;
+
+  localLeaderPrefixes = {
+    "insert" = "i";
+    "lists" = "l";
+    "mode" = "m";
+    "notes" = "n";
+    "todos" = "t";
+  };
 in {
   options = {
     enable = mkEnableOption "neorg";
   };
 
   config = mkIf cfg.self.enable {
-    plugins.neorg = {
-      enable = true;
-      modules = {
-        "core.defaults" = helpers.emptyTable;
-        "core.concealer" = helpers.emptyTable;
-        "core.dirman" = {
-          config = {
-            index = "index.norg";
-            workspaces = {
-              notes = "~/Workspace/neorg/notes";
+    extraPlugins = [nixpkgs.vimPlugins.neorg-telescope];
+
+    plugins = {
+      neorg = {
+        enable = true;
+        modules = {
+          "core.defaults" = helpers.emptyTable;
+          "core.concealer" = helpers.emptyTable;
+          "core.dirman" = {
+            config = {
+              index = "index.norg";
+              workspaces = {
+                notes = "~/Workspace/neorg/notes";
+              };
+              default_workspace = "notes";
             };
           };
-        };
-        "core.completion" = {
-          config.engine = "nvim-cmp";
+          "core.completion" = {
+            config.engine = "nvim-cmp";
+          };
+          "core.integrations.telescope" = helpers.emptyTable;
+          "core.keybinds" = {
+            config.hook = helpers.mkRaw ''
+              function(keybinds)
+                keybinds.map_event_to_mode("norg", {
+                  n = {
+                    { "<C-s>", "core.integrations.telescope.find_linkable" },
+                  },
+                  i = {
+                    { "<C-l>", "core.integrations.telescope.insert_link" },
+                  },
+                }, {
+                  silent = true,
+                  noremap = true,
+                })
+              end
+            '';
+          };
         };
       };
+
+      which-key.registrations = lib.mapAttrs' localLeaderKeymap localLeaderPrefixes;
     };
   };
 }
